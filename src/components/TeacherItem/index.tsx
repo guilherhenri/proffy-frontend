7b1f6003004cab50d32da01954d57485246ddd77
Guilherme Henrique <guilherhenri12@gmail.com>
2020-08-09 20:30:25 -0300

    Finish front-end web

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,37 +3,62 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-export default function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatasapp: number;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const createConnection = async (id: number) => {
+  try {
+    await api.post('/connections', {
+      user_id: id,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
         <img 
-          src="https://i.pinimg.com/236x/61/69/67/61696742e1b2d8b0d3ed70efaa1b0f89.jpg" 
-          alt="Guilherme Henrique"
+          src={teacher.avatar} 
+          alt={teacher.name}
         />
         <div>
-          <strong>Guilherme Henrique</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasta das melhores tecnologias de matemática avançada.
-        <br/><br/>
-        Apaixonado por calcular coisas na calculadora e por mudar a vida das pessoas através de cálculos.
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a onClick={() => createConnection(teacher.id)} href={`https://wa.me/${teacher.whatasapp}`} target="__blanck">
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   )
 }
+
+export default TeacherItem;
